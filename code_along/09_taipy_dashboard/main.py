a624da67ab4afe7f4c51e73cc38e428c6e4991d5
abdulrahmanfahmi <abdirahman3016@gmail.com>
2025-05-13 00:14:08 +0200

    skapa dashboard

--- code_along/09_taipy_dashboard/main.py
+++ code_along/09_taipy_dashboard/main.py
@@ -3,9 +3,9 @@ import taipy.gui.builder as tgb
 import pandas as pd
 from utils.constans import DATA_DIRECTORY
 
-df = pd.read_excel(DATA_DIRECTORY/"resultat-ansokningsomgang-2024 (1).xlsx", sheet_name="Tabell 3", skiprows=5)
+df = pd.read_excel(DATA_DIRECTORY / "ans_l14_ansokningar_yh_utb_beslut_2025.xlsx")
 
-with tgb.page() as page:
+with tgb.Page() as page:
     with tgb.part(class_name="container card"):
         tgb.text("MYH dashboard 2024", mode="md")
         
@@ -21,6 +21,5 @@ with tgb.page() as page:
                 
                 
 
-
 if __name__ == "__main__":
-    Gui(page).run(dark_mode=False, use_reloader=True, port=8081) 
+    Gui(page).run(dark_mode=False, use_reloader=True, port=8081)
